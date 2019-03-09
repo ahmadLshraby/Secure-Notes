1d7be64b7d804a512b1f1f667f224966b6b5fe63
Ahmad Shraby <ahmadelshraby10@outlook.com>
2019-03-09 13:51:48 +0200

    NoteVC
    UI and tableView

---
+++ Secure Notes/Controller/NoteVC.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Secure Notes
+//
+//  Created by sHiKoOo on 3/9/19.
+//  Copyright © 2019 sHiKoOo. All rights reserved.
+//
+
+import UIKit
+
+class NoteVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+
+    
+    
+    
+    
+}
+
+
+extension NoteVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "noteCell", for: indexPath) as? NoteCell {
+            
+            return cell
+        }else {
+            return UITableViewCell()
+        }
+    }
+    
+    
+}
---
+++ Secure Notes/View/NoteCell.swift
@@ -0,0 +1,24 @@
+//
+//  NoteCell.swift
+//  Secure Notes
+//
+//  Created by sHiKoOo on 3/9/19.
+//  Copyright © 2019 sHiKoOo. All rights reserved.
+//
+
+import UIKit
+
+class NoteCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
