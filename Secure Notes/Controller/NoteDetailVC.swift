2078377e21c15d785f25847e0ddeac1c3b20f266
Ahmad Shraby <ahmadelshraby10@outlook.com>
2019-03-09 23:53:36 +0200

    NoteDetailVC

---
+++ Secure Notes/Controller/NoteDetailVC.swift
@@ -0,0 +1,25 @@
+//
+//  NoteDetailVC.swift
+//  Secure Notes
+//
+//  Created by sHiKoOo on 3/9/19.
+//  Copyright © 2019 sHiKoOo. All rights reserved.
+//
+
+import UIKit
+
+class NoteDetailVC: UIViewController {
+
+    @IBOutlet weak var noteMessageTxt: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func lockNoteBtn(_ sender: UIButton) {
+        
+    }
+    
+
+
+}
